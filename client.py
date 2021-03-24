641365407bbb524f4e2faa40361858f50f68b181
lemonjie <wallowing0321@gmail.com>
2021-03-25 01:38:41 +0800

    add timing and annotation

--- client.py
+++ client.py
@@ -5,46 +5,64 @@ import hashlib
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #soc.connect(('127.0.0.1', 12345))
 soc.connect(('192.168.0.12', 12345))
+
 MAX_BUFFER_SIZE = 4096
 MD5_SIZE = 32
 ACK = 'ACK'
+send_size = 4000
 
+#read message which we want to send
 msg_file = open('test_50MB.txt', 'r')
 msg_string = msg_file.read()
-msg_bytes = msg_string.encode('utf-8')
 msg_file.close()
 
+#calculate md5 od message
 m = hashlib.md5()
+msg_bytes = msg_string.encode('utf-8') #calculate md5 should use bytes in python3
 m.update(msg_bytes)
 msg_md5 = m.hexdigest()
 
-send_size = 4000
-num_send = int(len(msg_bytes) / send_size) + (len(msg_bytes) % send_size > 0)
 
+#test how many time does ACK cost
+time_start = time.time()
+soc.send('ACK'.encode('utf-8'))
+soc.recv(MAX_BUFFER_SIZE)
+time_end = time.time()
+ack_time = (time_end - time_start)/2
+print('an ACK cost ', ack_time, 'sec')
+
+#start calculate time
+time_start = time.time()
+#send length of the whole message in th begining
 soc.send(str(len(msg_bytes) + MD5_SIZE).encode('utf-8'))
 recv_bytes = soc.recv(MAX_BUFFER_SIZE)
-if recv_bytes.decode('utf-8') != ACK:
-    print('ACK wrong. ', recv_bytes.decode('utf-8'))
 
+#calculate how many times should client send
+num_send = int(len(msg_bytes) / send_size) + (len(msg_bytes) % send_size > 0)
 for i in range(0, num_send):
-    print('sending ', i)
-    if ((i+1)*send_size) >= len(msg_bytes):
+    #keep send message and receive ACK
+    if ((i+1)*send_size) >= len(msg_bytes): #send the end of the whole message
         soc.send(msg_bytes[i*send_size:])
     else:
-        try:
-            soc.send(msg_bytes[i*send_size : (i+1)*send_size])
-        except socket.error as msg:
-            print('Error : ')
-
+        soc.send(msg_bytes[i*send_size : (i+1)*send_size])
+    #print('sending ', i) #just want to see it run
     recv_bytes = soc.recv(MAX_BUFFER_SIZE)
-    if recv_bytes.decode('utf-8') != ACK:
-        print('ACK wrong. ', recv_bytes.decode('utf-8'))
+    #if recv_bytes.decode('utf-8') != ACK:
+    #    print('ACK wrong. ', recv_bytes.decode('utf-8'))
 
+#send md5
 soc.send(msg_md5.encode('utf-8'))
+soc.recv(MAX_BUFFER_SIZE)
+time_end = time.time()
+
+#receive result and close the connection
 result_bytes = b''
 while len(result_bytes) < 17:
     result_bytes += soc.recv(MAX_BUFFER_SIZE)
 result_string = result_bytes.decode("utf8")
 soc.close()
-
 print('Result from server :\n', result_string[-17:])
+
+#calculate time
+send_time = time_end - time_start - ack_time
+print('send time: ', send_time, ' sec')
--- server.py
+++ server.py
@@ -1,17 +1,22 @@
 def do_some_stuffs_with_input(input_string):
-    import hashlib
-    print('now write txt')
+
+    #write message(client send) to .txt
     file_write = open('recv_from_client.txt', 'w')
-    file_write.write(input_string[:-32])
+    file_write.write(input_string[:-32]) #final 32 bytes are md5 of the whole message, calculate by client
     file_write.close()
-    print('now calculat md5')
+
+    #calculate md5 of message which receive from client
+    import hashlib
     m = hashlib.md5()
     m.update(input_string[:-32].encode('utf-8'))
     md5 = m.hexdigest()
+
+    #check if the two md5 are the same
     if md5 == input_string[-32:]:
         ifsuccess = 'send successfully'
     else:
         ifsuccess = 'something failed.'
+
     return ifsuccess, md5
 
 def client_thread(conn, ip, port, MAX_BUFFER_SIZE = 4096):
@@ -22,45 +27,55 @@ def client_thread(conn, ip, port, MAX_BUFFER_SIZE = 4096):
     input_from_client_bytes = b''
     input_from_client_bytes_num = b''
     recv_num = 0
+    
+    #for test how many time ACK cost
+    conn.recv(MAX_BUFFER_SIZE)
+    conn.send('ACK'.encode('utf-8'))
 
+    #receive the size of the whole message(bytes) in the begining
     input_from_client_bytes_num = conn.recv(MAX_BUFFER_SIZE)
-    send_num = int(input_from_client_bytes_num.decode('utf-8'))
     conn.send(ACK.encode('utf-8'))
-    print(send_num)
+    send_num = int(input_from_client_bytes_num.decode('utf-8'))
+    
     while True:
+
+        #keep receive message and send ACK
         input_from_client_bytes_part = conn.recv(MAX_BUFFER_SIZE)
-        print('recv ')
         conn.send(ACK.encode('utf-8'))
-        print('send ACK')
+
+        #print(input_from_client_bytes_part.decode('utf-8', 'ignore')[:100]) # just want to see it run
+        
         siz = sys.getsizeof(input_from_client_bytes_part)
-        print(input_from_client_bytes_part.decode('utf-8', 'ignore')[:100])#
         if  siz > MAX_BUFFER_SIZE:
             print('The length of input is probably too long: ', siz)
+        
+        #sum each part of the whole message
         input_from_client_bytes += input_from_client_bytes_part
+        #check if length of receive is the same as length of send from client
         recv_num += len(input_from_client_bytes_part)
         if recv_num >= send_num:
             break
-    print('get all msg.')
+    
+    #decode message from byte to string
     input_from_client = input_from_client_bytes.decode("utf8")
-
+    #write .txt and check md5
     res, md5 = do_some_stuffs_with_input(input_from_client)
-    print('string md5: ', md5)
+    print(res)
 
+    #send resule to client
     conn.send(res.encode('utf-8'))
-    #conn.close()
-    #print('Connection ' + ip + ':' + port + " ended")
 
 def start_server():
 
     import socket
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # this is for easy starting/killing the app
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     soc.settimeout(None)
     print('Socket created')
 
     try:
         soc.bind(("192.168.0.12", 12345))
+        #soc.bind(("127.0.0.1", 12345))
         print('Socket bind complete')
     except socket.error as msg:
         import sys
